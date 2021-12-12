e6cc3d199d138f029c2c68f06ed0c04be79dc6a1
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2021-12-12 15:43:36 -0600

    Create keyboard input system

--- src/principal/input/Keyboard.java
+++ src/principal/input/Keyboard.java
@@ -1,4 +1,34 @@
 package principal.input;
 
-public class Keyboard {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener {
+    private final static int numberOfKeys = 120;
+    private final boolean[] keys = new boolean[numberOfKeys];
+
+    public boolean up, down, left, right, run, exit;
+
+    public void update() {
+        up = keys[KeyEvent.VK_W];
+        down  = keys[KeyEvent.VK_S];
+        left = keys[KeyEvent.VK_A];
+        right = keys[KeyEvent.VK_D];
+        run = keys[KeyEvent.VK_SHIFT];
+        exit = keys[KeyEvent.VK_ESCAPE];
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+    }
 }
