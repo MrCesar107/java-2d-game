c71ce7970d2c7d160f1762114ee6386253f84b1e
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2021-12-12 01:54:27 -0600

    Create game main loop

--- src/principal/MainGestor.java
+++ src/principal/MainGestor.java
@@ -1,12 +1,19 @@
 package principal;
 
 import jdk.jfr.internal.tool.Main;
+import principal.graphics.CanvasGame;
+import principal.graphics.WindowGame;
+import principal.statesmachine.StatesController;
 
 public class MainGestor {
     private boolean isRunning = false;
     private String title;
     private int width;
     private int height;
+
+    private CanvasGame canvas;
+    private WindowGame window;
+    private StatesController statesController;
 
     private MainGestor(final int width, final int height, final String title) {
         this.width = width;
@@ -25,6 +32,59 @@ public class MainGestor {
     }
 
     private void init() {
+        canvas = new CanvasGame();
+        window = new WindowGame();
+        statesController = new StatesController();
+        initGameProcess();
+    }
+
+    private void initGameProcess() {
+        int aps = 0;
+        int fps = 0;
+
+        final int nanoSeconds = 1000000000;
+        final int targetAPS = 60;
+        final double nanoSecondsPerUpdate = nanoSeconds / targetAPS;
+
+        long updateReference = System.nanoTime();
+        long referenceCounter = System.nanoTime();
+
+        double timeElapsed;
+        double delta = 0;
+
+        while (isRunning) {
+            final long startLoop = System.nanoTime();
+
+            timeElapsed = startLoop - updateReference;
+            updateReference = startLoop;
+
+            delta += timeElapsed / nanoSecondsPerUpdate;
+
+            while (delta >= 1) {
+                update();
+                aps++;
+                delta--;
+            }
+
+            draw();
+            fps++;
+
+            if (System.nanoTime() - referenceCounter > nanoSeconds) {
+                System.out.println("APS: " + aps);
+                System.out.println("FPS: " + fps);
+
+                aps = 0;
+                fps = 0;
+                referenceCounter = System.nanoTime();
+            }
+        }
+    }
+
+    private void update() {
+
+    }
+
+    private void draw() {
 
     }
 }
